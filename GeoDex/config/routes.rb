bed12a1e83c37680dfb6009eaa4f84084fd47ace
Mart van de Ven <m@type.hk>
2013-05-18 21:23:19 +0800

    Index Page

--- GeoDex/config/routes.rb
+++ GeoDex/config/routes.rb
@@ -1,6 +1,9 @@
 GeoDex::Application.routes.draw do
+  get "pages/index"
+
   resources :features
 
+  root :to => "pages#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
