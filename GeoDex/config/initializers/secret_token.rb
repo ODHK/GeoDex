c0b0c71426be013086081f2f8902421ebea7e935
Mart van de Ven <m@type.hk>
2013-05-18 18:22:22 +0800

    Basic Rails with Mongoid

---
+++ GeoDex/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeoDex::Application.config.secret_token = '883e7dfe452cc275037d6ffa49fe6ec0b3796a45f6980bcc636e93f719f6720e6bdc75124ba2eba1efc43a35e513f1c6f6fccd8477187e91e93c9d81f442235b'
