044843c87ff6e6a265e123f2bb82bbe047fe59b5
Matt Nodurfth <mnodurft@gmail.com>
2013-05-19 16:04:05 +0800

    Added angular js files into assets/javascript

---
+++ GeoDex/app/assets/javascripts/directives.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/* Directives */
+
+
+angular.module('myApp.directives', []).
+  directive('appVersion', ['version', function(version) {
+    return function(scope, elm, attrs) {
+      elm.text(version);
+    };
+  }]);
+
+
+angular.module('myModule', ['ui.bootstrap']);
+
+
+
